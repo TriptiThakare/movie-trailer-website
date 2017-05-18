7ea0333dc38bf26c360a9109691b915e1209ccae
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2017-05-18 12:44:55 -0700

    Added comments.

--- media.py
+++ media.py
@@ -9,10 +9,12 @@ import webbrowser
 
 class Movie():
     """ This class provides a way to store movie related information"""
+## This function initializes the class variables 
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+# This function opens the browser with the url given to the function
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
